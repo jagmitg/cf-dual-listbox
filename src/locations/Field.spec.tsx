29102736a270858baf310dfc1c824d1dead018a6
Jagmit Gabba <jagmitg@gmail.com>
2025-03-09 17:20:47 +0000

    field spec update

--- src/locations/Field.spec.tsx
+++ src/locations/Field.spec.tsx
@@ -1,17 +1,18 @@
-import { render } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
-import { mockCma, mockSdk } from '../../test/mocks';
-import Field from './Field';
+import { render } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { mockCma, mockSdk } from "../../test/mocks";
+import Field from "./Field";
 
-vi.mock('@contentful/react-apps-toolkit', () => ({
-  useSDK: () => mockSdk,
-  useCMA: () => mockCma,
+// Mock Contentful hooks to return our test SDK & CMA
+vi.mock("@contentful/react-apps-toolkit", () => ({
+    useSDK: () => mockSdk,
+    useCMA: () => mockCma
 }));
 
-describe('Field component', () => {
-  it('Component text exists', () => {
-    const { getByText } = render(<Field />);
+describe("Field component", () => {
+    it("Component text exists", () => {
+        const { getByText } = render(<Field />);
 
-    expect(getByText('Hello Entry Field Component (AppId: test-app)')).toBeTruthy();
-  });
+        expect(getByText("Hello Entry Field Component (AppId: test-app)")).toBeTruthy();
+    });
 });
